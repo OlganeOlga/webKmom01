e8993ff4e0d346484ba0d084fdf44cea94923a4e
Ольга Егорова <107860466+OlganeOlga@users.noreply.github.com>
2022-09-04 07:56:13 +0200

    Add files via upload

--- report/public/about.php
+++ report/public/about.php
@@ -5,12 +5,12 @@ include('../view/header.php'); ?>
 <main>
     <h1>Om kursen och webbplatsen</h1>
     
-    <img src="img/bradska.png" width="300" class="brådska" alt="representerar kurs">
+    <img src="img/webbtelescope.jpg" width="300" class="background" alt="representerar kurs">
     
     <p>Den sida tillhör kursen ’Wbbtec’, och det är förkortning till webbteknologier. Vad kan jag skriva mera? Det är myket att lära och det är inte alltid tydligt hur jag löser de oändliga frågor som uppstår dagligen. Ibland blir jag trött på att sita framför dator och inte komma en millimeter fram. Desto roligare blir det när jag hittar lösningen.<br>
     
     
 
-    <p>Just nu håller vi på att skapa en <a href="...............">webbsida</a>. Jag lärde mig lite om struktur av html fil, om hur skapas referenser, hur kan man skapa en länk och hur kan jag ändra textens stil, storlek och färg.</p>
+    <p>Just nu håller vi på att skapa en <a href="https://github.com/OlganeOlga/webKmom01">webbsida</a>. Jag lärde mig lite om struktur av html fil, om hur skapas referenser, hur kan man skapa en länk och hur kan jag ändra textens stil, storlek och färg.</p>
 </main>
 <?php include('../view/footer.php') ?>
